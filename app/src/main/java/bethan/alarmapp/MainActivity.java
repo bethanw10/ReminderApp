8e7b25e9f6ae2d47817ef0bcaae54a1ab04f3ee7
Bethan <bethanw10@gmail.com>
2018-11-13 08:47:31 +0000

    Clear calendar after save. Update selected dates on calender when the month is changed

--- app/src/main/java/bethan/alarmapp/MainActivity.java
+++ app/src/main/java/bethan/alarmapp/MainActivity.java
@@ -1,23 +1,20 @@
 package bethan.alarmapp;
 
-import android.provider.CalendarContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.Switch;
-import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import org.threeten.bp.DayOfWeek;
 import org.threeten.bp.LocalDate;
-import org.threeten.bp.LocalDateTime;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 // TODO: Butter knife?
@@ -30,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
     Button saveBtn;
 
+    WeekdayRule weekdayRule;
+    LocalDate currentMonthStartDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,15 +37,30 @@ public class MainActivity extends AppCompatActivity {
 
         getViews();
 
+        weekdayRule = new WeekdayRule(new ArrayList<DayOfWeek>());
+        currentMonthStartDate = LocalDate.now();
+
+        calendar.state().edit()
+                .setMinimumDate(CalendarDay.today())
+                .commit();
+
         calendar.setSelectionMode(MaterialCalendarView.SELECTION_MODE_NONE);
+
+        calendar.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView materialCalendarView,
+                                       CalendarDay calendarDay) {
+                currentMonthStartDate = calendarDay.getDate();
+                updateCalendar(weekdayRule);
+            }
+        });
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 List<DayOfWeek> daysOfWeek = getSelectedDayOfWeeks();
 
-                WeekdayRule weekdayRule = new WeekdayRule(daysOfWeek);
-
+                weekdayRule = new WeekdayRule(daysOfWeek);
                 updateCalendar(weekdayRule);
             }
         });
@@ -54,10 +69,14 @@ public class MainActivity extends AppCompatActivity {
     private void updateCalendar(Rule rule) {
         List<CalendarDay> datesToSelect = new ArrayList<>();
 
-        // TODO: hard coded
-        for (LocalDate date = LocalDate.of(2018, 11, 1); date.isBefore(LocalDate.of(2018, 11, 30)); date = date.plusDays(1))
-        {
-            if(rule.isTrueForDate(date)) {
+        LocalDate startDate = LocalDate.now().isBefore(currentMonthStartDate) ?
+                LocalDate.now() :
+                currentMonthStartDate.minusMonths(1);
+
+        LocalDate endDate = currentMonthStartDate.plusMonths(2);
+
+        for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
+            if (rule.isTrueForDate(date)) {
                 datesToSelect.add(CalendarDay.from(date));
             }
         }
@@ -68,19 +87,13 @@ public class MainActivity extends AppCompatActivity {
     private List<DayOfWeek> getSelectedDayOfWeeks() {
         List<DayOfWeek> daysOfWeek = new ArrayList<>();
 
-        if(mondaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.MONDAY);
-
-        if(tuesdaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.TUESDAY);
-
-        if(wednesdaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.WEDNESDAY);
-
-        if(thursdaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.THURSDAY);
-
-        if(fridaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.FRIDAY);
-
-        if(saturdaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.SATURDAY);
-
-        if(sundaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.SUNDAY);
+        if (mondaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.MONDAY);
+        if (tuesdaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.TUESDAY);
+        if (wednesdaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.WEDNESDAY);
+        if (thursdaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.THURSDAY);
+        if (fridaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.FRIDAY);
+        if (saturdaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.SATURDAY);
+        if (sundaySwitch.isChecked()) daysOfWeek.add(DayOfWeek.SUNDAY);
 
         return daysOfWeek;
     }
@@ -100,6 +113,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setDatesSelected(MaterialCalendarView calendar, List<CalendarDay> dates) {
+        calendar.clearSelection();
+
         for (CalendarDay date : dates) {
             calendar.setDateSelected(date, true);
         }
